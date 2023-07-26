40edeec093de9c8991ebd459cfdc3ad58d43fbe7
Laura <138983570+lauramill0214@users.noreply.github.com>
2023-07-26 23:37:15 +0400

    Update fibo.java

--- fibo.java
+++ fibo.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+        int a = 0, b = 1, c;
+        System.out.print("Fibonacci Series: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        scanner.close();
+    }
+}
