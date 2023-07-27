daf0ca06db7572e95f0dc7434409bbffc7f32c9b
Laura <138983570+lauramill0214@users.noreply.github.com>
2023-07-27 23:15:07 +0400

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,1 +1,15 @@
 import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        System.out.println("Factorial of " + n + " is " + factorial);
+        scanner.close();
+    }
+}
