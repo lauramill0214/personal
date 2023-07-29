8a807a4eb84e24ed5de2e77b6442caa312c58219
Laura <138983570+lauramill0214@users.noreply.github.com>
2023-07-30 00:48:32 +0400

    Create armstrong_num_prog.java

---
+++ armstrong_num_prog.java
@@ -0,0 +1,20 @@
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int sum = 0;
+        int temp = number;
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, 3);
+            temp /= 10;
+        }
+        if (number == sum) {
+            System.out.println(number + " is an Armstrong number");
+        } else {
+            System.out.println(number + " is not an Armstrong number");
+        }
+        scanner.close();
+    }
+}
